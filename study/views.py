d97805c72799d889ecf450fcb1f5f3251b9626ae
awmleer <awmleer@sparker.xyz>
2018-06-20 22:13:54 +0800

    generate learn list

--- study/views.py
+++ study/views.py
@@ -2,13 +2,19 @@ from django.http import JsonResponse
 from .models import WordRecord
 from bank.models import Repo
 from neuron.utils.decorator import require_login, json_request
+from neuron.utils.response import ErrorResponse
 
 
 @require_login
 def generate_learn_list(request):
+    amount = int(request.GET.get('amount'))
+    if amount<5:
+        return ErrorResponse('单词数量过少')
+    elif amount>100:
+        return ErrorResponse('单词数量过多')
     all_entries = Repo.objects.get(id=request.GET['repo_id']).entries
     learned_entries = request.user.learned_entries
-    entries = all_entries.difference(learned_entries)
+    entries = all_entries.difference(learned_entries)[:amount]
     res = []
     for entry in entries:
         res.append(entry.as_dict())
